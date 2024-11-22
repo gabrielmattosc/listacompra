dcf5bb512b19ca936cd7ad609335c5f7e8fa1378
gabrielmattosc <gabrielmattoscosta5@gmail.com>
2024-11-22 14:51:17 -0300

    adicinonando lista de comprados

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
-const listaComprados = document.getElementById("lista-comprados")
+const listaComprados = document.getElementById("lista-comprados");
 let contador = 0;
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
@@ -33,11 +33,16 @@ function adicionarItem(evento) {
     checkboxLabel.addEventListener("click", function (evento) {
       const checkboxInput = evento.currentTarget.querySelector(".checkbox-input");
       const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+      const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
 
       if (checkboxInput.checked){
-        checkboxCustomizado.classList.add("checked")
+        checkboxCustomizado.classList.add("checked");
+        itemTitulo.style.textDecoration = "line-through";
+        listaComprados.appendChild(itemDaLista)
       } else {
-        checkboxCustomizado.classList.remove("checked")
+        checkboxCustomizado.classList.remove("checked");
+        itemTitulo.style.textDecoration = "none";
+        listaDeCompras.appendChild(itemDaLista)
       }
     })
 
@@ -53,6 +58,7 @@ function adicionarItem(evento) {
   
     // Cria o parágrafo com o texto do item
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
     nomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(nomeDoItem);
   
@@ -78,7 +84,7 @@ function adicionarItem(evento) {
     imagemEditar.alt = "Editar item";
     botaoEditar.appendChild(imagemEditar);
   
-  
+    
     // Adiciona os botões ao container
     containerBotoes.appendChild(botaoRemover);
     containerBotoes.appendChild(botaoEditar);
